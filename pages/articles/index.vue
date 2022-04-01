61000f1bc1fc5872601762b9e15d2ccfc07c2def
Linh Nguyen <linhvn09@gmail.com>
2022-04-01 17:10:16 +0700

    add quill editor

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="ui main text container">
-
+    <client-only>
+      <quill-editor v-model="editor" :options="editorOptions" />
+    </client-only>
     <ArticlesDirectory
       :articles="articlesList"
     />
@@ -8,11 +10,78 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import ArticlesDirectory from '~/components/ArticlesDirectory'
+import { mapGetters } from 'vuex';
+import axios from 'axios';
 
+
+import ArticlesDirectory from '~/components/ArticlesDirectory';
+
+export const uploadImage = function() {
+      let fileInput = this.container.querySelector('input.ql-image[type=file]');
+      if (fileInput == null) {
+        fileInput = document.createElement('input');
+        fileInput.setAttribute('type', 'file');
+        fileInput.setAttribute('accept', 'image/png, image/gif, image/jpeg, image/bmp, image/x-icon');
+        fileInput.classList.add('ql-image');
+        fileInput.addEventListener('change', () => {
+          if (fileInput.files != null && fileInput.files[0] != null) {
+            var formData = new FormData();
+            formData.append("file", fileInput.files[0]);
+            axios.post(`${process.env.API_SERVER_URL}/api/photos`,
+
+      formData, {headers: {'Authorization': JSON.parse(localStorage.getItem('user'))}}
+      ).then((response) => {
+              let imageUrl = response.data.opt_web.url;
+              console.log(imageUrl)
+              // let range = this.quill.getSelection(true);
+              // this.quill.updateContents(new Delta()
+              //   .retain(range.index)
+              //   .delete(range.length)
+              //   .insert({ image: imageUrl })
+              // , null);
+              fileInput.value = "";
+            });
+          }
+        });
+        this.container.appendChild(fileInput);
+      }
+      fileInput.click();
+    }
+
+const editorOptions = {
+        theme: 'snow',
+        debug: 'info',
+        placeholder: 'Type your post...',
+        readOnly: true,
+        modules: {
+          toolbar: {
+            container: [
+              ['bold', 'italic', 'underline', 'strike'],
+              [{ 'header': 1 }, { 'header': 2 }],
+              ['blockquote', 'code-block'],
+              [{ 'list': 'ordered'}, { 'list': 'bullet' }], // List
+				      [{ 'indent': '-1'}, { 'indent': '+1' }],// Indent
+				      [{ 'direction': 'rtl' }],
+				      [{ 'size': ['small', false, 'large', 'huge'] }],
+              [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+              [{ 'color': [] }, { 'background': [] }],
+              [{ 'font': [] }],
+				      [{ 'align': [] }],
+				      ['clean'],
+              ['link', 'image', 'video']
+            ],
+            handlers: { image: uploadImage }
+          }
+        }
+      }
 export default {
   components: { ArticlesDirectory },
+  data() {
+    return {
+      editorOptions: editorOptions,
+      editor: ''
+    }
+  },
   fetch: ({ store }) => {
     return store.dispatch('getArticles');
   },
---
+++ plugins/quill-editor.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueQuillEditor from 'vue-quill-editor'
+
+// require styles
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+const editorOption = {
+  modules: {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'],
+      ['blockquote', 'code-block'],
+      [{ list: 'ordered' }, { list: 'bullet' }],
+      [{ indent: '-1' }, { indent: '+1' }],
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      [{ color: [] }, { background: [] }],
+      ['clean'],
+    ],
+  },
+};
+
+Vue.use(VueQuillEditor, /* { default global options } */)
